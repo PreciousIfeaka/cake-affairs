d522f1ca85b22c3646d6cfde4ad47e1b6b08b07a
PreciousIfeaka <preciousifeaka@gmail.com>
2026-07-05 14:30:08 +0100

    chore: added vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CataloguePage from './pages/CataloguePage';
 import AdminPage from './pages/AdminPage';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"       element={<CataloguePage />} />
-        <Route path="/admin"  element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Analytics />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CataloguePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
